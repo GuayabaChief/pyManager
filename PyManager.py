ab1ee4cf4858b2fa809d4e85c6d509db71c45ff9
Gerardo Suárez <73609331+GuayabaChief@users.noreply.github.com>
2024-01-25 16:10:50 -0600

    Add files via upload

--- PyManager.py
+++ PyManager.py
@@ -1,14 +1,42 @@
 import pyfiglet
 import pandas as pd
 
-acsiiTitle = pyfiglet.figlet_format("Py Manager",font='univers')
+acsiiTitle = pyfiglet.figlet_format("Py Manager",font='univers', width=100)
 print(acsiiTitle)
-print("\n Created with <3 by Gerardo Suarez. \nTotally free for all \n")
+print("\nCreated with <3 by Gerardo Suarez. \nTotally free for all \n")
 
 print("Primero que nada, escribe la ruta de tu archivo: \n")
 path = input()
 
 file = pd.read_csv(path)
 
-print("\n Tu archivo es: " + path)
+print("\n Tu archivo es: " + path + "\n")
+print("Archivo valido! \n")
 
+while True:
+
+    def case1():
+        return
+    def case2():
+        return
+    def case3():
+        return
+    def case4():
+        return
+        
+    switch_dict = {
+        1: case1,
+        2: case2,
+        3: case3,
+        4: case4
+    }
+
+    numero = int(input("Introduce el número de la opcion que quieras realizar: \n1. Alta(Sube algo) \n2. Baja(Elimina) \n3. Cambios (Cambia algo) \n4. Consulta (Busca algo) \n5. Salir\n"))
+    
+
+    if numero == 5:
+        print("Adios!")
+        break
+    else:
+         print("a")
+        
